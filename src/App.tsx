4810d9355dacd01ef26bec5cc305f84badd56740
Archit5655 <architgargagra@gmail.com>
2026-05-08 12:29:57 +0530

    chnage

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,8 @@ function App() {
         <Abstract />
         <Methodology />
         <Results />
-        <Conclusion />
         <ResultsGallery />
+        <Conclusion />
       </main>
       <Footer />
       
